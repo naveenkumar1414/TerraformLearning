da92726b9ebc463d58ca7707f5a4ebe443f64c91
naveenkumar1414 <naveen.kumar2@thoughtworks.com>
2024-07-12 12:42:03 +0530

    uodated the main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "my_security_group" {
 }
 resource "aws_key_pair" "my_key" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa MIIEogIBAAKCAQEAqn4k6A5woK7b/LBQx1w1whJNyvJBrpZEW0d8hadVjyAVczB1uPdz9tnSGWLNi8GYny1wUErTH62U+XA39E7ZBShL1R9YwmrFmfCenc/45N8iaDyyOpnc6uvUcc9AIpQXGJSQ8f/RJMvNPjbGUmnqErnTBdzvT7H+QEf0FNKYMaFSGaaJE4dLg6XVe7khW4i+Ughc8RmqyAzTQUkIIi03kjVN/cCLXRz2srTYlzh+xJU+tA5t3J4nY7bPhdAqhs1HRR9+olkxtTN7qICHzvHDw71ydwXwZeywQWCp5TyVP9r1w30IdGaDxijw1OdMRk9yiebZEwG/eDhh+hvQA9sXJQIDAQABAoIBACq2A0TnKSv34kCBYxsubIE72KU4319/YYyEchuBehuq6/S5dSRrSW/TzaBycg/K5hMqbPKQbCeJcsKybDIV9qQmHik7+x5pZStVbkcWrMGZbKM0YWnw1bk8bzI7u1NuSgLsZQtbDOfs7nPhgPlwcyVRzB2x5XnPm2Q7Ilh9EYeM+yDLHJgnWlsxoPWP37YRm1iISJMD+6Ci3sf830izb4iTmlLCby1gmgjJSJiBEp3S4njH5G/wE1GUFadWDd/iXNUUtUhmU0aY9eciiOrMRhMLLdaw+SR650OmqHI+Xz5sOwVwpVSmm5uAbjwGqjJDoQXgs52MlL6v30VqJqOdfIECgYEA6HGnKDE5qHoFJoo2ncfBtRi7v0sW7ApO8jxKJ2alXdgE4grEWIpElMtxxY7C3fv9ZDYqr46kxYR4+iHG71I7MJU81b9kijLifqiyiJ87LZ8wR/wc6KXyAVOEzDaypdgnHjypW2EUcUUqQKR+5mdAOvhazjwFSr0LJDrzUNE7O1ECgYEAu8VG3Vsdqnt8Qa7KoCzQdo6owEZrTJ/jvzuDTUuQdDgRAQpUxAZ12JqmgK8Ocx1E3WWvJV/lzz6FmlW6AHQHbXwAD8/YX9S0RkCB32tCMH1JAyhRqNSXjJ3eq81uQCme7NzPVE1LhPPBkbbFa5BRvxOIFveKYCkIaXrHIrSyQZUCgYAEdGkntAnO2MfQfUWesH1d4yN1/xJpY6kvq0ggiS8SJVmym1I4AC5gS6Xai2ngpzur9/3Jb4po3qkS4iLV41WPEM0hq5FZtK/yjXkedS7FKQ/b0VEchFGnBNq+ry2tT/8v7SmxvqKNqS2uH3jvIXtxpeDXov7W99iG0TpoihoAgQKBgHCvSYHaBomjJcS9QvixLEGaB4esOwR3B9RhwMCIUp6MbxfyvUAOqHZfCQE9DSzkKcm8FkoVrPKckM04GTHgb/yyZ88VasLIAZAPcmFEGyrCZSix75+kRXar30+3LyzuTWPUd9Aduu6E4muyBfQxXf2PmSI/WBmxRk0W057uVfOpAoGAUdey+dgbQuo1Mmnbx3pI0kWnuGFGpfkfUgaJUf5LONeu+gc4V+GAhM08DI8yocrGzOcRdHTDMVY7AkbqWIx+s/VBzeeMW78xRUWrMD8k5PetFNa+5gUfVLHuskndi7swI4e/cjbLXGXJ1Hjdb4acOwOvYesTz70Bx/uCZR+GxRA= naveen.kumar2@thoughtworks.com"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDWEkhPcokWN9mIA76PqXImND6Go8yKshn6DZbCVoRt6QFAShbYTse85v0sS//NXseRRaxH56njXJ+jEwf+SFzLEgLAl/AcAzAM4HGFkoyIvBEHOFJ8MpKm4M0Siut/nZVFHvyy7BrZq3GdjjKKuCAF/K7ynLngAZFnThbXU0bQ4PFKg3c9TjcVZRAaYb5oImkHGhvEfZSYrxjODqzAQCTpe1W7w6QsT4VrizxCFRuDgZOOdlpDuywDdqF9D+38HvCeWSoaRCKNovtFkKKezoyWQIO8sdjnkafysWlgfwCNoSdZCsorSOnykKwTW1QXzs3VdhctSZz+fEspB/3/CzhsLiApwyOrPttt91GjmsjtU4TuTcPvT0s+EwUKEihYFPrN6Y1rH0UAEUAHWCAlKNm9NsHHxWTfiVawzO/Bl77NwAQk2im275sQjnjWDYYtG2m0sFqDvsXMPAN2mYjzjCLdLYcRP+9KB2/cnxaY74ybaTuJr6qZZpaI314pJpQh1gVeaki4EnQ4xRXyGmNeslqg56GQoftTctKi8w3rcEH0WbRy0n3TtmwF3yeg10MO7cMP0leimQIRZPvK09BCUeeJ/DEnPypCukn4k+AoKujDjIjOczUHIFUcVGTIL+fdeLygiPS+2804W5Fe+OCRrfEZdWZ+VT8gDm1kqvbA7QlnhQ== naveen.kumar2@thoughtworks.com"
 }
 
 # Create EC2 Instance
@@ -75,11 +75,12 @@ resource "aws_instance" "my_terraform_ec2_Instance" {
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.my_subnet.id
   security_groups = [aws_security_group.my_security_group.id]
-  key_name        = aws_key_pair.my_key.key_name
+  key_name        = aws_key_pair.my_key.key_nam
   provisioner "remote-exec" {
     inline = [
       "chmod +x build.sh",
-      "./build.sh"
+      "./build.sh",
+      "chmod +x ./ssh/id_rsa"
     ]
     connection {
       type        = "ssh"
