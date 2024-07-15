86adbee34ced9e97d3ec8fe8e385fc2dc733ec76
naveenkumar1414 <naveen.kumar2@thoughtworks.com>
2024-07-15 15:16:03 +0530

    updated the key

--- main.tf
+++ main.tf
@@ -64,28 +64,24 @@ resource "aws_security_group" "my_security_group" {
     Name = "my-terraform-learning-security-group"
   }
 }
-resource "aws_key_pair" "my_key" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDWEkhPcokWN9mIA76PqXImND6Go8yKshn6DZbCVoRt6QFAShbYTse85v0sS//NXseRRaxH56njXJ+jEwf+SFzLEgLAl/AcAzAM4HGFkoyIvBEHOFJ8MpKm4M0Siut/nZVFHvyy7BrZq3GdjjKKuCAF/K7ynLngAZFnThbXU0bQ4PFKg3c9TjcVZRAaYb5oImkHGhvEfZSYrxjODqzAQCTpe1W7w6QsT4VrizxCFRuDgZOOdlpDuywDdqF9D+38HvCeWSoaRCKNovtFkKKezoyWQIO8sdjnkafysWlgfwCNoSdZCsorSOnykKwTW1QXzs3VdhctSZz+fEspB/3/CzhsLiApwyOrPttt91GjmsjtU4TuTcPvT0s+EwUKEihYFPrN6Y1rH0UAEUAHWCAlKNm9NsHHxWTfiVawzO/Bl77NwAQk2im275sQjnjWDYYtG2m0sFqDvsXMPAN2mYjzjCLdLYcRP+9KB2/cnxaY74ybaTuJr6qZZpaI314pJpQh1gVeaki4EnQ4xRXyGmNeslqg56GQoftTctKi8w3rcEH0WbRy0n3TtmwF3yeg10MO7cMP0leimQIRZPvK09BCUeeJ/DEnPypCukn4k+AoKujDjIjOczUHIFUcVGTIL+fdeLygiPS+2804W5Fe+OCRrfEZdWZ+VT8gDm1kqvbA7QlnhQ== naveen.kumar2@thoughtworks.com"
-}
 
 # Create EC2 Instance
 resource "aws_instance" "my_terraform_ec2_Instance" {
-  ami             = "ami-01376101673c89611"
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.my_subnet.id
-  security_groups = [aws_security_group.my_security_group.id]
-  key_name        = aws_key_pair.my_key.key_name
+  ami                         = "ami-01376101673c89611"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.my_subnet.id
+  security_groups             = [aws_security_group.my_security_group.id]
+  key_name                    = "terraform-learning-key"
+  associate_public_ip_address = true
   provisioner "remote-exec" {
     inline = [
       "chmod +x build.sh",
       "./build.sh",
-      "chmod +x ./ssh/id_rsa"
     ]
     connection {
       type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("./ssh/id_rsa")
+      user        = "ec2-user"
+      private_key = file("/Users/naveenkumarduraisamy/Downloads/terraform-learning-key.pem")
       host        = self.public_ip
     }
   }
